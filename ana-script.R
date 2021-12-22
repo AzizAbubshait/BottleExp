6e3c5a6591b46862c2f8c65e34b3365cc5854386
Giulia Siri <giulia.siri@iit.it>
2021-12-22 15:27:09 +0100

    yuawfbkjq3

--- ana-script.R
+++ ana-script.R
@@ -8,7 +8,7 @@ cbbPalette = c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00",
 
 # To do  ----
 # read in data ----
-myPath = paste(getwd(), "/data/", sep = "")
+myPath = paste(getwd(), "/pilot_20_01/", sep = "")
 filenames = list.files(myPath)
 
 all_dat = lapply(paste(myPath, filenames, sep = ""), read.csv)
@@ -22,52 +22,52 @@ str(all_dat)
 summary(all_dat)
 
 # here we look at which trials are valid
+# all_dat %>%
+#   group_by(pid) %>%
+#   summarize(no_touch_onGo = sum(TOUCH_TIME == -1 &
+#                                   #is.na(SOUND_DELAY_MS_LEFT) == F &
+#                                   Action == "go"),
+#             not_sure = sum(RELEASE_TIME == -1), # <---- we don't know what this is. 
+#             early_release = sum(is.na(SOUND_DELAY_MS_LEFT)),
+#             touch_wrong_bottle = sum(BOTTLE_RESPONSE_POSITION != -1 &
+#                                        BOTTLE_RESPONSE_POSITION != ï..Holder)) %>% 
+#   print(n = Inf)
+
 all_dat %>%
   group_by(pid) %>%
-  summarize(no_touch_onGo = sum(TOUCH_TIME == -1 &
-                             #is.na(SOUND_DELAY_MS_LEFT) == F &
-                             Action == "go"),
-            not_sure = sum(RELEASE_TIME == -1), # <---- we don't know what this is. 
-            early_release = sum(is.na(SOUND_DELAY_MS_LEFT)),
-            touch_wrong_bottle = sum(BOTTLE_RESPONSE_POSITION != -1 &
-                                       BOTTLE_RESPONSE_POSITION != ï..Holder)) %>% 
+  summarize(valid_trials = sum(VALIDITY == "True"),
+            invalid_trials = sum(VALIDITY == "False")) %>%
   print(n = Inf)
 
-
-# here we look at which trials are correct
 clean_dat = all_dat %>%
-    # filter(TOUCH_TIME != -1 | Action == "go",
-    #        RELEASE_TIME != -1,
-    #        is.na(SOUND_DELAY_MS_LEFT) == F,
-    #        BOTTLE_RESPONSE_POSITION == -1 | BOTTLE_RESPONSE_POSITION == ï..Holder
-    #        ) %>%
-  mutate(agent = ifelse(ï..Holder == 'right', 'clamp', ifelse(
-    ï..Holder == "left", 'iCub', NA)),
+  group_by(pid) %>%
+  filter(VALIDITY == "True") %>%
+  mutate(
+    agent = ifelse(ï..Holder == 'right', 'clamp', ifelse(
+      ï..Holder == "left", 'iCub', NA)),
     corr_new = ifelse(Action == "stop" | TOUCH_TIME > 0, "incor", 
                       ifelse(Action == "stop" |
                       TOUCH_TIME == -1, "cor", NA))) %>%
-  group_by(pid, Action, agent) %>%
-  summarize(error_rate = sum(corr_new == "cor")/length(corr_new),
+  group_by(pid, agent) %>%
+  summarize(error_rate = sum(CORRECT == 1)/length(CORRECT),
             rt = mean(RELEASE_TIME),
             touch_rt = mean(TOUCH_TIME))
 
 clean_dat %>%
-  ggplot(aes(y = rt, agent, color = Action))+
+  ggplot(aes(y = rt, agent))+
   stat_summary(fun.data = mean_se, geom = "point", position = position_dodge(.2), size = 3)+
   stat_summary(fun.data = mean_se, geom = "errorbar", position = position_dodge(.2))+
   theme_bw()
 
 clean_dat %>%
-  ggplot(aes(y = touch_rt, agent, color = Action))+
+  ggplot(aes(y = touch_rt, agent))+
   stat_summary(fun.data = mean_se, geom = "point", position = position_dodge(.2), size = 3)+
   stat_summary(fun.data = mean_se, geom = "errorbar", position = position_dodge(.2))+
   theme_bw()
 
 clean_dat %>%
-  ggplot(aes(y = error_rate, agent))+
+  ggplot(aes(y = error_rate, agent, color = pid))+
+  geom_point()+
   stat_summary(fun.data = mean_se, geom = "point", position = position_dodge(.2), size = 3)+
   stat_summary(fun.data = mean_se, geom = "errorbar", position = position_dodge(.2))+
-  theme_bw()+
-  facet_wrap(~Action, scales = "free_y")
-
-fhuftyuiu
+  theme_bw()
